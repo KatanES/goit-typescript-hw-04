9ce22ee1e4ec78aa2f40d9875a89387a7b825d1d
KatanES <businesskatan.es@gmail.com>
2023-10-15 20:09:23 +0200

    Final

--- src/homework/4.tsx
+++ src/homework/4.tsx
@@ -1,91 +1,49 @@
-import React, { createContext, useMemo, useState, useContext } from "react";
-import noop from "lodash/noop";
-
-type MenuIds = "first" | "second" | "last";
-type Menu = { id: MenuIds; title: string };
-
-// Додати тип Menu Selected
-
-const MenuSelectedContext = createContext<MenuSelected>({
-  selectedMenu: {},
-});
-
-// Додайте тип MenuAction
-
-const MenuActionContext = createContext<MenuAction>({
-  onSelectedMenu: noop,
-});
-
-type PropsProvider = {
-  children; // Додати тип для children
-};
-
-function MenuProvider({ children }: PropsProvider) {
-  // Додати тип для SelectedMenu він повинен містити { id }
-  const [selectedMenu, setSelectedMenu] = useState<SelectedMenu>({});
-
-  const menuContextAction = useMemo(
-    () => ({
-      onSelectedMenu: setSelectedMenu,
-    }),
-    []
-  );
-
-  const menuContextSelected = useMemo(
-    () => ({
-      selectedMenu,
-    }),
-    [selectedMenu]
-  );
-
-  return (
-    <MenuActionContext.Provider value={menuContextAction}>
-      <MenuSelectedContext.Provider value={menuContextSelected}>
-        {children}
-      </MenuSelectedContext.Provider>
-    </MenuActionContext.Provider>
-  );
+class Key {
+  private signature: number;
+  constructor() {
+    this.signature = Math.random();
+  }
+  getSignature(): number {
+    return this.signature;
+  }
 }
 
-type PropsMenu = {
-  menus; // Додайте вірний тип для меню
-};
-
-function MenuComponent({ menus }: PropsMenu) {
-  const { onSelectedMenu } = useContext(MenuActionContext);
-  const { selectedMenu } = useContext(MenuSelectedContext);
-
-  return (
-    <>
-      {menus.map((menu) => (
-        <div key={menu.id} onClick={() => onSelectedMenu({ id: menu.id })}>
-          {menu.title}{" "}
-          {selectedMenu.id === menu.id ? "Selected" : "Not selected"}
-        </div>
-      ))}
-    </>
-  );
+class Person {
+  constructor(private key: Key) {}
+  getKey(): Key {
+    return this.key;
+  }
 }
 
-export function ComponentApp() {
-  const menus: Menu[] = [
-    {
-      id: "first",
-      title: "first",
-    },
-    {
-      id: "second",
-      title: "second",
-    },
-    {
-      id: "last",
-      title: "last",
-    },
-  ];
+abstract class House {
+  protected door: boolean = false;
+  private tenants: Person[] = [];
 
-  return (
-    <MenuProvider>
-      <MenuComponent menus={menus} />
-    </MenuProvider>
-  );
+  constructor(protected key: Key) {}
+
+  comeIn(person: Person): void {
+    if (this.door) {
+      this.tenants.push(person);
+    }
+  }
+  abstract openDoor(key: Key): void;
 }
+
+class MyHouse extends House {
+  openDoor(key: Key): void {
+    if (this.key.getSignature() === key.getSignature()) {
+      this.door = true;
+    }
+  }
+}
+
+const key = new Key();
+
+const house = new MyHouse(key);
+const person = new Person(key);
+
+house.openDoor(person.getKey());
+
+house.comeIn(person);
+
+export {};
